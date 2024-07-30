566df4e4122c1515fe7142011f26e9432c574bf3
Mathias Jave Diaz <u202212721@upc.edu.pe>
2024-07-30 14:35:54 -0500

    chore: configured gitignore file

--- Banking/obj/Debug/net8.0/Banking.AssemblyInfo.cs
+++ Banking/obj/Debug/net8.0/Banking.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Banking")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b2841952a939b5428a09cbcd248b8d3d74ac4500")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Banking")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Banking")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
